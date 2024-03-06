ec304149d61132712b0d8c6d9a9184465e6494f9
MatteoRuffa <cortese_17@protonmail.com>
2024-03-06 14:54:21 +0100

    add small correction

--- js/mr-script.js
+++ js/mr-script.js
@@ -27,6 +27,7 @@ console.log(userAge);
 
 let ticketPrice = numberKilometers * 0.21;
 console.log(ticketPrice);
+document.getElementById('price').innerHTML = 'Il prezzo del tuo biglietto è: ' + ticketPrice.toFixed(2) + '&euro;';
 
 if (userAge < 18) {
     ticketPrice = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2); 
@@ -36,6 +37,4 @@ if (userAge < 18) {
     ticketPrice = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2); 
     console.log(ticketPrice);
     document.getElementById('price').innerHTML = 'Il prezzo del tuo biglietto è: ' + ticketPrice + '&euro;';
-} else {
-    document.getElementById('price').innerHTML = 'Il prezzo del tuo biglietto è: ' + ticketPrice.toFixed(2) + '&euro;';
 }
